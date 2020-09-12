b5029efb4a8fe5b3f6de9ada28fe2841ebbe969d
teshukatepalli <teshukatepalli996@gmail.com>
2020-09-12 19:11:18 +0530

    React samle homepage design with symantic-ui

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Home from "./components/home";
+import "./App.css";
+// import { Button } from "semantic-ui-react";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
